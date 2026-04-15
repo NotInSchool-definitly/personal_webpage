96132278920dfa1e787212344beebf24eba949f6
NotInSchool-definitly <28staiga@pittsfield.net>
2026-04-15 10:51:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// let yourname = document.querySelector('button')
+// yourname.addEventListener('click', inputMsg,);
+
+// function inputMsg(){
+//     let name = prompt('Please enter your name for no reason at all') 
+//     yourname.textContent = 'Hi ' + name;
+// }
+
+console.log("Script loaded successfully?")
+
+let getName = prompt("Social security number here");
+console.log(getName)
+let card = prompt('credit card information')
+console.log(card)
